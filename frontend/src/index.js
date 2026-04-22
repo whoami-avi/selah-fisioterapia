03dbe7a8d079bfa244abfd28d06ef6b7b9d0cde6
Abi_Lopez <76602132+whoami-avi@users.noreply.github.com>
2026-04-22 15:44:47 -0600

    Delete frontend directory
    Signed-off-by: Abi_Lopez <76602132+whoami-avi@users.noreply.github.com>

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-// Static app - Selah Fisioterapia runs from public/ folder
-const App = () => null;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
